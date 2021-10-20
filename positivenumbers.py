5dc1cf23d059a4ddcd42e7f022bb2b196b1b3fa1
Harishabaskar <92776110+Harishabaskar@users.noreply.github.com>
2021-10-21 00:26:58 +0530

    Delete positivenumbers.py

--- positivenumbers.py
+++
@@ -1,8 +0,0 @@
-list1=[12,-7,5,64,-14]
-print("original numbers in the list1:",list1)
-pos_nos1=[num for num in list1 if num>=0]
-print("positive numbers in the list2:",*pos_nos1)
-list2=[12,14,-95,3]
-print("original numbers in the list2:",list2)
-pos_nos2=[num for num in list2 if num>=0]
-print("positive numbers in the list2:",pos_nos2)
